56965fa4fe772fd0a5cd1d9a1d78aaa70da372c3
lucho19jose <barbozagonzalesjose@gmail.com>
2023-03-29 15:39:52 -0500

    mutations and actionsç

--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,6 @@
 import { createStore } from 'vuex'
+import { COMMIT_UPDATE_USERNAME } from '@/common/mutation-types.js'
+import { getUser } from '@/api'
 
 const store = createStore({
   state() {
@@ -12,8 +14,17 @@ const store = createStore({
     }
   },
   mutations: {
-    updateUsername(state, username) {
+    [COMMIT_UPDATE_USERNAME](state, username) {
       state.username = username;
+    }
+  },
+  actions: {
+    async updateUsername({ commit }, username) {
+      /* console.log('updateusername action', username);
+      commit('updateUsername', username) */
+      const user = await getUser(1);
+      console.log(user)
+      commit(COMMIT_UPDATE_USERNAME, username)
     }
   }
 })
